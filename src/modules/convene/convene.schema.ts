0d5d36ecbf186143b8ddc11dbdee572906d21d02
trongtindev <me@trongtin.dev>
2024-06-23 20:14:58 +0700

    chore: temp update, reinstall OS :)

--- src/modules/convene/convene.schema.ts
+++ src/modules/convene/convene.schema.ts
@@ -55,3 +55,34 @@ export class ConveneChunk extends Document<Types.ObjectId> {
   updatedAt: Date;
 }
 export const ConveneChunkSchema = SchemaFactory.createForClass(ConveneChunk);
+
+// convene_summary
+export type ConveneSummaryDocument = HydratedDocument<ConveneSummary>;
+@Schema({ collection: 'convene_summary' })
+export class ConveneSummary extends Document<Types.ObjectId> {
+  @Prop({ unique: true })
+  banner: string;
+
+  @Prop({ default: 0 })
+  totalPull: number;
+
+  @Prop({ default: 0 })
+  totalUsers: number;
+
+  @Prop({ default: [] })
+  pullByDay: { time: string; total: number }[];
+
+  @Prop({ default: [] })
+  fiveStarList: { name: string; total: number }[];
+
+  @Prop({ default: [] })
+  fourStarList: { name: string; total: number }[];
+
+  @Prop({ default: () => new Date(), index: true })
+  createdAt: Date;
+
+  @Prop({ default: () => new Date(), index: true })
+  updatedAt: Date;
+}
+export const ConveneSummarySchema =
+  SchemaFactory.createForClass(ConveneSummary);
